267d54858cdfb64ab190128f7f313ed45fa7d26d
Gabriel <gsechaus@gmail.com>
2021-07-25 09:33:43 -0300

    ✨ Added a basic command handler using Discord.js documentation

---
+++ commands/saymyname.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'saymyname',
+	description: 'Command replys with the username and nickname (if not available with undefined) in chat.',
+	execute(message, args) {
+		let nickname = message.member.nickname;
+        let username = message.author.username; 
+        message.reply(`${nickname} ${username}`);
+	},
+};
--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-require('dotenv').config();
+require('dotenv').config(); 
+const fs = require('fs');
 const Discord = require('discord.js');
+const config = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const prefix = config.prefix; 
+
+client.on("message", message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) {
+        message.reply('This command does not exist, please type **pugs!help** to see the full list of commands.');
+        return;  
+    } 
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+}); 
 
 client.login(process.env.TOKEN);
 
 client.once('ready', () => {
     console.log('OWPugs is ready for usage.');
 });
-
