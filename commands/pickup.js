c61cae59509d11780257f6841675c5fbf9b38946
Gabriel <gsechaus@gmail.com>
2021-08-24 16:06:20 -0300

    :sparkles: Add new players to json or updates data if in json already

--- commands/pickup.js
+++ commands/pickup.js
@@ -7,20 +7,49 @@ module.exports = {
 	name: 'pickup',
 	description: 'Pickup players from voice channel and sends message with teams',
 	execute(message, args) {
-		if (!message.member.voice.channel) return embed.sendReply(message, `You cannot start picking up players since you are not in a voice channel.`);
-		const voiceInfo = message.member.voice.channel.members.array();
-		if (voiceInfo.length <= 0) return embed.sendReply(message, `You need atleast 12 players to start the PUG.`);
+		try {
+			if (!message.member.voice.channel) return embed.sendReply(message, `You cannot start picking up players since you are not in a voice channel.`);
+			const voiceInfo = message.member.voice.channel.members.array();
+			if (voiceInfo.length <= 0) return embed.sendReply(message, `You need atleast 12 players to start the PUG.`);
+			let players = getJsonPlayers();
+			console.log("Validating Players:");
+			const serverID = message.guild.id;
 
-		let players = getJsonPlayers(); // JSOn
+			voiceInfo.map((voiceUser) => {
+				const playerID = voiceUser.id;
+				const playerName = voiceUser.nickname ? voiceUser.nickname : voiceUser.user.username;
+				const playerRoles = getPlayerRoles(message, voiceUser);
 
-		console.log(players);
+				const indexUserID = players.findIndex(player => player.user_id === playerID);
+				const indexServerID = players.findIndex(player => player.server_id === serverID);
+				const notFound = -1; // If not found returns -1
 
-		voiceInfo.map((voiceUser) => {
-			const playerName = voiceUser.nickname ? voiceUser.nickname : voiceUser.user.username;
-			const playerRoles = getPlayerRoles(message, voiceUser);
-		});
+				if (indexUserID != notFound && indexServerID != notFound) {
+					console.log(`Found in Server and User: ${playerName}`);
+					players[indexUserID].name = playerName;
+					players[indexUserID].roles = playerRoles;
+					players[indexUserID].active = true;
+				} else {
+					console.log(`Not found in Server and User: ${playerName}`);
+					players.push({
+						user_id: playerID,
+						server_id: serverID,
+						name: playerName,
+						roles: playerRoles,
+						wins: 0,
+						losses: 0,
+						matches_without_playing: 0,
+						active: true
+					});
+				}
+			});
 
-		chooseMap(message);
+			players = makeAllInactive(players);
+			setJsonPlayers(players);
+			chooseMap(message);
+		} catch (err) {
+			console.log(err);
+		}
 	},
 };
 
@@ -56,8 +85,17 @@ const getJsonPlayers = () => {
 
 const setJsonPlayers = (players) => {
 	try {
-		const data = fs.writeFileSync('./players.json', JSON.stringify(players, null, 2))
+		fs.writeFileSync('./players.json', JSON.stringify(players, null, 2))
 	} catch (err) {
 		console.error(err)
 	}
+}
+
+const makeAllInactive = (players) => {
+	players.map((player) => {
+		player.active = false;
+		return player;
+	});
+
+	return players;
 }
