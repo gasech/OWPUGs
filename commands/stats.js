0202e812da4c70f0c83b74fe4e1a002028c19dc1
Gabriel <gsechaus@gmail.com>
2021-08-26 16:46:25 -0300

    :bug: Better stats find algorithm

--- commands/stats.js
+++ commands/stats.js
@@ -6,15 +6,16 @@ module.exports = {
 	description: 'Replies with your stats or anyone that you target',
 	execute(message, args) {
 		const players = getJsonPlayers(); // json
+		const serverID = message.guild.id;
 
 		if (args == "") {
 			const playerID = message.author.id;
-			sendStats(message, playerID, players);
+			sendStats(message, playerID, players, serverID);
 		} else {
 			if (args.length !== 1) return embed.sendReply(message, `Please use **pugs!stats @user**`);
 			const taggedUser = args[0];
 			const playerID = taggedUser.replaceAll(/[<@!>]/ig, '');
-			sendStats(message, playerID, players);
+			sendStats(message, playerID, players, serverID);
 		}
 	},
 };
@@ -32,17 +33,18 @@ const getJsonPlayers = () => {
 	return players;
 }
 
-const sendStats = (message, playerID, players) => {
-	try {
-		let index = players.findIndex(player => player.user_id === playerID);
+const sendStats = (message, playerID, players, serverID) => {
+	const indexUser = players.findIndex(player => player.user_id === playerID && player.server_id === serverID);
+	const notFound = -1; // If not found returns -1
 
+	if (indexUser != notFound) {
 		embed.sendReply(message,
-			`**${players[index].name}'s stats:**\n
-			**Roles:** ${players[index].roles}
-			**Wins:**	${players[index].wins}
-			**Losses:**	${players[index].losses}`
+			`**${players[indexUser].name}'s stats:**\n
+			**Roles:** ${players[indexUser].roles}
+			**Wins:**	${players[indexUser].wins}
+			**Losses:**	${players[indexUser].losses}`
 		);
-	} catch (err) {
+	} else {
 		embed.sendReply(message, `Could not find user stats, please play atleast one match to have your stats registered.`);
 	}
 }
