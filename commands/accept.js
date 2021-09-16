ef3ab643593e07472ad1e9c772c32fd63bbacf2b
Gabriel <gsechaus@gmail.com>
2021-09-16 00:36:20 -0300

    :sparkles: Added accept command

---
+++ commands/accept.js
@@ -0,0 +1,45 @@
+const embed = require('../embeds');
+const editJSON = require('../jsonEditor');
+
+module.exports = {
+	name: 'accept',
+	description: 'Accepts the match',
+	execute(message, args, pugState) {
+		if (!pugState.acceptMatchPeriod) return embed.sendReply(message, "You must be in accept match period, please type **pugs!pickup**.");
+
+		embed.sendMessage(message, `Match accepted, please create the lobby with the selected players in both teams, after finishing the map please type **pugs!winner team1** or **pugs!winner team2**`);
+		
+		let players = editJSON.readPlayers();
+
+		for (let i = 0; i < players.length; i++) {
+			if (!findPlayerInTeams(players[i], pugState)) {
+				pugState.players[i].matchesWithoutPlaying++;
+			} else {
+				pugState.players[i].matchesWithoutPlaying = 0;
+			}
+		}
+
+		editJSON.writePlayers(players);
+
+		pugState.acceptMatchPeriod = false;
+		pugState.pugsRunning = true;
+	}
+};
+
+const findPlayerInTeams = (playerI, pugState) => {
+	let found = false;
+
+	for (let player of pugState.teams[0]) {
+		if (playerI.name == player) {
+			found = true;
+		}
+	}
+
+	for (let player of pugState.teams[1]) {
+		if (playerI.name == player) {
+			found = true;
+		}
+	}
+
+	return found;
+}
