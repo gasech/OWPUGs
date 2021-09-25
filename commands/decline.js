5cf70decd95622181dc02f21b25d074ad69f930b
Gabriel <gsechaus@gmail.com>
2021-09-25 14:07:28 -0300

    :art: Added few improvements to decline command.

--- commands/decline.js
+++ commands/decline.js
@@ -5,7 +5,22 @@ module.exports = {
   description: 'Declines the match',
   execute(message, args, pugState) {
     if (!pugState.acceptMatchPeriod) return embed.sendReply(message, "You must be in accept match period, please type **pugs!pickup**");
-    embed.sendMessage(message, "Match declined with success");
+
     pugState.acceptMatchPeriod = false;
+    pugState.teams = [["", "", "", "", "", ""], ["", "", "", "", "", ""]];
+    
+    let players = editJSON.readPlayers();
+    players = makeAllInactive(players);
+    editJSON.writePlayers(players);
+
+    embed.sendMessage(message, "Match declined with success!");
   },
 };
+
+const makeAllInactive = (players) => { 
+	players.map((player) => {
+		player.active = false;
+		return player;
+	});
+	return players;
+}
