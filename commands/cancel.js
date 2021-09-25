bef264478e05185fd21deecdd4aaf90abc25e8f4
Gabriel <gsechaus@gmail.com>
2021-09-25 14:05:47 -0300

    :art: Improved cancel command
    Added reduce matches without playing method and a few improvements

--- commands/cancel.js
+++ commands/cancel.js
@@ -6,6 +6,22 @@ module.exports = {
   description: 'Cancels the match',
   execute(message, args, pugState) {
     if (!pugState.pugsRunning) return embed.sendReply(message, "There must be a match running.");
+    embed.sendMessage(message, "Match canceled with success!");
     pugState.pugsRunning = false;
+    pugState.teams = [["", "", "", "", "", ""], ["", "", "", "", "", ""]];
+
+    let players = editJSON.readPlayers();
+    players = reduceMatchesWithoutPlaying(players);
+    editJSON.writePlayers(players);
   },
 };
+
+const reduceMatchesWithoutPlaying = (players) => { 
+	players.map((player) => {
+		if(player.matches_without_playing > 0){
+      player.matches_without_playing--;
+    }
+		return player;
+	});
+	return players;
+}
